263e4bc029485ee3b369c37a73af7e723e0e5683
GideonBature <infoaboutgideon@gmail.com>
2025-01-14 22:09:36 +0100

    Feat: add code

--- enums2.rs
+++ enums2.rs
@@ -1,4 +1,24 @@
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
 
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+    Resize {width: u8, height: u8},
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{self:?}");
+    }
+}
 
 fn main() {
     let messages = [
