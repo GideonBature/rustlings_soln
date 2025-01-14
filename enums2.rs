71980d620492246627018646b10bae2c64812888
GideonBature <infoaboutgideon@gmail.com>
2025-01-14 22:10:02 +0100

    Feat: remove code

--- enums2.rs
+++ enums2.rs
@@ -26,7 +26,10 @@ fn main() {
             width: 10,
             height: 30,
         },
-
+        Message::Move(Point { x: 10, y: 15 }),
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
     ];
 
     for message in &messages {
